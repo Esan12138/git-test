eed054ca516fc2b7520c7d35bb0b096f5c1c3a86
Esan <1255294160@qq.com>
2022-10-17 21:11:34 +0800

    first commit

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,10 @@
+/**
+ * @Description:
+ * @Date:2022-10-17-21:10
+ * @author:Esan
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
