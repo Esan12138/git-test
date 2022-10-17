0e1352a450db46da9b155eab8d1939789038fb76
Esan <1255294160@qq.com>
2022-10-17 21:53:23 +0800

    master commit1

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("master ");
         System.out.println("hello git4");
         System.out.println("hot-fix commit 2");
+        System.out.println("push test");
     }
 }
